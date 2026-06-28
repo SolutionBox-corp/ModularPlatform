4b7a6fbc69466b57b3d46193f69106bca528d0b3
Miroslav Lalík <lalikspse@gmail.com>
2026-06-28 18:24:04 +0200

    test: cover jobs graceful shutdown wiring

--- tests/ModularPlatform.Jobs.Tests/JobsHostWiringTests.cs
+++ tests/ModularPlatform.Jobs.Tests/JobsHostWiringTests.cs
@@ -1,4 +1,7 @@
 using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using ModularPlatform.Jobs;
 using Quartz;
 using Shouldly;
@@ -16,4 +19,31 @@ public sealed class JobsHostWiringTests
         jobType.ShouldNotBeNull();
         jobType.GetCustomAttribute<DisallowConcurrentExecutionAttribute>().ShouldNotBeNull();
     }
+
+    [Fact]
+    public void Jobs_host_waits_for_running_jobs_on_shutdown()
+    {
+        using var host = JobsHostBuilder.Create(BootArgs()).Build();
+
+        var options = host.Services.GetRequiredService<IOptions<QuartzHostedServiceOptions>>().Value;
+
+        options.WaitForJobsToComplete.ShouldBeTrue();
+    }
+
+    private static string[] BootArgs() =>
+    [
+        "--environment=Development",
+        "--ConnectionStrings:Write=Host=localhost;Port=5432;Database=jobswiring;Username=postgres;Password=postgres",
+        "--ConnectionStrings:Read=Host=localhost;Port=5432;Database=jobswiring;Username=postgres;Password=postgres",
+        "--Modules:Identity:Enabled=true",
+        "--Modules:Billing:Enabled=true",
+        "--Modules:Notifications:Enabled=true",
+        "--Modules:Gdpr:Enabled=true",
+        "--Modules:Operations:Enabled=true",
+        "--Modules:Files:Enabled=true",
+        "--Modules:Marketing:Enabled=true",
+        "--Modules:Tenancy:Enabled=true",
+        "--Billing:Stripe:UseFakeGateway=true",
+        "--Storage:Provider=local",
+    ];
 }
